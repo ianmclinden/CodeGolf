0fd4649bcf8df07ec7ca74cb023da70bccf0deb9
Ian McLinden <mcli0027@umn.edu>
2017-04-16 20:57:22 -0500

    Initial Commit

---
+++ CodeGolf/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CodeGolf
+//
+//  Created by Ian McLinden on 4/16/17.
+//  Copyright © 2017 Ian McLinden. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
